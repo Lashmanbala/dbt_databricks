ffbc9b3cc2bc80bcd31c53fadd81127f1fe46130
lashmanbala <lashmanbala@gmail.com>
2025-02-15 04:41:27 +0000

    Executing seed and test together

--- dags/dbt_dag.py
+++ dags/dbt_dag.py
@@ -6,10 +6,14 @@ from airflow.operators.python import PythonOperator
 # Function to check if seed has already run
 import os
 
-def seed_check():
-    """ For the first time alone the seed cmd will be called. In the consecutive calls dbt run cmds only called"""
-    if not os.path.exists('/tmp/dbt_seed_done'): 
+def seed_and_test():
+    """Run dbt seed and dbt test only if not already run."""
+    if not os.path.exists('/tmp/dbt_seed_done'):
+        # Run dbt seed
         os.system('dbt seed --project-dir /home/ubuntu/dbt_databricks/dbt_databricks_project --select orders order_items')
+        # Run dbt test immediately after seeding
+        os.system('dbt test --project-dir /home/ubuntu/dbt_databricks/dbt_databricks_project --select orders order_items')
+        # Create a flag file to indicate seed completion
         with open('/tmp/dbt_seed_done', 'w') as f:
             f.write('done')
 
@@ -18,22 +22,23 @@ with DAG(
     default_args={'owner': 'airflow', 'retries': 0},
     schedule_interval='@daily',
     start_date=days_ago(1),
-    catchup=False,         # No backfill, only current and future runs
+    catchup=False, # No backfill, only current and future runs
+    # paused_on_creation=True,  # DAG stays paused initially
 ) as dag:
 
-    seed_task = PythonOperator(
+    seed_and_test_task = PythonOperator(
         task_id='dbt_seed_check',
-        python_callable=seed_check,
+        python_callable=seed_and_test,
     )
 
     run_silver = BashOperator(
         task_id='dbt_run_silver',
-        bash_command='dbt run --project-dir /home/ubuntu/dbt_databricks/dbt_databricks_project --select silver --vars \'{target_date: "201310"}\''
-    )
+        bash_command='dbt run --project-dir /home/ubuntu/dbt_databricks/dbt_databricks_project --select silver --vars \'{target_date: "201308"}\''
+    ) # update the date variable for the consecutive runs
 
     run_gold = BashOperator(
         task_id='dbt_run_gold',
         bash_command='dbt run --project-dir /home/ubuntu/dbt_databricks/dbt_databricks_project --select gold'
     )
 
-    run_silver >> run_gold
+    seed_and_test_task >> run_silver >> run_gold
